e45a9df681c9096c99efe362e53fb0dd8ad7815e
Srijan Gulati <srijangulati06@gmail.com>
2026-07-21 10:32:17 -0700

    Time: 0 ms (100.00%) | Memory: 8.5 MB (87.53%) - LeetSync

---
+++ 69-sqrtx/sqrtx.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+       int s=0,e=x;
+       int ans=-1;
+
+       while(s<=e){
+        long long int mid= s+(e-s)/2;
+
+        if(mid*mid <= x){
+            ans=mid;
+            s=mid+1;
+        }
+        else{
+            e=mid-1;
+        }
+       } 
+
+       return ans;
+    }
+};
